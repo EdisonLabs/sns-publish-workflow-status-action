0df6f522f1bea5a8fbeb1cbfb76df5606449dc82
Alberto Castillo G <me@betoissues.com>
2021-07-19 09:25:04 -0500

    Initial action for getting workflow run conclusions and sending the payload to SNS (#1)
    * Add node dependencies.
    
    Signed-off-by: Alberto Castillo G <Alberto.Castillo@pfizer.com>
    
    * Initial action
    
    Signed-off-by: Alberto Castillo G <Alberto.Castillo@pfizer.com>
    
    * Update README
    
    * Reduce inputs and other feedback changes.
    
    Signed-off-by: Alberto Castillo G <Alberto.Castillo@pfizer.com>
    
    * Update action specification
    
    * Updated README, package.json and added LICENSE
    
    Co-authored-by: jkribeiro <me@jeankribeiro.com>

--- README.md
+++ README.md
@@ -1,1 +1,75 @@
-# sns-publish-workflow-status-action
+# AWS SNS Publish Workflow Status Action
+
+This repository contains an action that resolves the current Workflow Run conclusion and sends a SNS (Amazon Simple Notification Service) message to the topic provided.
+
+## Inputs
+
+| Name | Type | Description |
+| ---- | ---- | ----------- |
+| `GITHUB_TOKEN` | string | Secret GitHub API token to use for making API requests. |
+| `RUN_ID` | string | Run ID for the workflow. Default: `github.run_id` |
+| `INITIAL_JOB` | boolean | Used to send a "started" status as initial notification. Default: `false` |
+| `GITHUB_CONTEXT` | string | GitHub context object. Automatically assigned to be included in the notification. |
+| `TOPIC_ARN` | string | AWS ARN for the SNS topic. |
+| `AWS_REGION` | string | AWS SNS region. Can be stored in environment. |
+| `AWS_ACCESS_KEY_ID` | string | AWS access key ID. Can be stored in environment. |
+| `AWS_SECRET_ACCESS_KEY` | string | AWS secret access key. Can be stored in environment. |
+
+## Outputs
+
+| Name | Type | Description |
+| ---- | ---- | ----------- |
+| `MESSAGE_ID` | string | ID of the SNS message sent. |
+
+## Example usage
+
+```yaml
+on: push
+
+env:
+  AWS_REGION: ${{ secrets.AWS_REGION }}
+  AWS_ACCESS_KEY_ID: ${{ secrets.AWS_ACCESS_KEY_ID }}
+  AWS_SECRET_ACCESS_KEY: ${{ secrets.AWS_SECRET_ACCESS_KEY }}
+jobs:
+  initial-notification:
+    runs-on: ubuntu-latest
+    name: Send initial notification
+    steps:
+      - name: Send initial notification
+        uses: EdisonLabs/sns-publish-workflow-status-action@1.0.0
+        with:
+          TOPIC_ARN: ${{ secrets.TOPIC_ARN }}
+          INITIAL_JOB: true
+  failed-run:
+    runs-on: ubuntu-latest
+    name: Failed test run
+    needs: [initial-notification]
+    steps:
+      - name: Failed step
+        run: exit 1
+  final-notification:
+    runs-on: ubuntu-latest
+    name: Send final notification
+    if: always()
+    needs: [failed-run]
+    steps:
+      - name: Determine status and send notification
+        uses: EdisonLabs/sns-publish-workflow-status-action@1.0.0
+        with:
+          TOPIC_ARN: ${{ secrets.TOPIC_ARN }}
+```
+
+## Message format
+This action publishes the following message format to SNS. 
+
+```
+{
+  "github": <context object>,
+  "workflow": {
+    "status": "<workflow status>"
+  }
+}
+```
+
+* `<context object>`: [GitHub context object](https://docs.github.com/en/actions/reference/context-and-expression-syntax-for-github-actions#contexts) containing  information about workflow runs, runner environments, jobs, and steps.
+* `<workflow status>`: The workflow conclusion status, such as: `started`, `neutral`, `skipped`, `success`, `cancelled`, `timed_out`, `action_required` and `failure`. 
---
+++ src/index.js
@@ -0,0 +1,112 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const AWS = require('aws-sdk');
+
+const CONCLUSIONS = [
+	'neutral',
+	'skipped',
+	'success',
+	'cancelled',
+	'timed_out',
+	'action_required',
+	'failure',
+];
+
+// Fetch all the workflow run jobs
+const getJobs = async (context, runId, token) => {
+	const octokit = github.getOctokit(token);
+	const jobs = await octokit.paginate(octokit.rest.actions.listJobsForWorkflowRun, {
+		...context.repo,
+		run_id: runId,
+	});
+
+	return jobs;
+}
+
+// Make a unique list of the run conclusions
+const getConclusions = jobs => Object.values(jobs
+	.filter((job, index, s) => s.indexOf(job) === index && job.conclusion !== null)
+	.map(job => ({ name: job.name, conclusion: job.conclusion}))
+	.reduce((acc, job) => ({...acc, [job.name]: job.conclusion}), {}));
+
+// Match the correct conclusion for the run
+const getConclusion = async (context, runId, token) => {
+	const IS_INITIAL = core.getBooleanInput('INITIAL_JOB');
+
+	if (IS_INITIAL) return "started";
+
+	const jobs = await getJobs(context, runId, token);
+
+	core.startGroup("Jobs information:");
+	core.info(JSON.stringify(jobs, null, 2));
+	core.endGroup();
+
+	const jobConclusions = getConclusions(jobs);
+
+	return CONCLUSIONS
+		.filter(conclusion => jobConclusions.includes(conclusion))
+		.slice(-1)[0];
+}
+
+
+// Send notification to SNS
+const sendNotification = async message => {
+	const AWS_REGION = core.getInput("AWS_REGION") || process.env.AWS_REGION;
+	const AWS_ACCESS_KEY_ID = core.getInput("AWS_ACCESS_KEY_ID") || process.env.AWS_ACCESS_KEY_ID;
+	const AWS_SECRET_ACCESS_KEY = core.getInput("AWS_SECRET_ACCESS_KEY") || process.env.AWS_SECRET_ACCESS_KEY;
+	const TOPIC_ARN = core.getInput("TOPIC_ARN");
+
+	AWS.config.update({region: AWS_REGION});
+
+	const params = {
+		Message: JSON.stringify(message),
+		TopicArn: TOPIC_ARN
+	};
+
+	const awsSnsPromise = new AWS.SNS({ apiVersion: "2010-03-31" }).publish(params).promise();
+
+	core.info(`Message ${params.Message} sent to the topic ${params.TopicArn}`);
+
+	return awsSnsPromise;
+}
+
+async function main() {
+	try {
+		const ghContext = core.getInput('GITHUB_CONTEXT');
+		const ghObject = JSON.parse(ghContext);
+		const token = ghObject.token;
+		const runId = ghObject.run_id;
+		delete ghObject.token;
+		const context = github.context;
+		const conclusion = await getConclusion(context, runId, token);
+
+		core.startGroup("Conclusion information:");
+		core.info("CONCLUSION: " +  conclusion);
+		core.endGroup();
+
+		core.startGroup("Sending SNS Notification:");
+		const message = {
+			github: ghObject,
+			workflow: {
+				status: conclusion,
+			}
+		};
+
+		const snsResult = await sendNotification(message);
+
+		if (snsResult.err) {
+			core.error(err.Message);
+			core.setFailed(err.Message);
+		} else {
+			core.info("Message published!");
+			core.info("Message ID: " + snsResult.MessageId);
+			core.setOutput('MESSAGE_ID', snsResult.MessageId);
+		}
+		core.endGroup();
+	} catch (error) {
+		core.error(error.message);
+		core.setFailed(error.message);
+	}
+}
+
+main();
